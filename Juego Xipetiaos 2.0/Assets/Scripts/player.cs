6078274726cc03ca377747650b2163f58dcdb807
BaljeetTM <nicolas.tapia08@alumnos.ucn.cl>
2022-11-05 02:02:05 -0300

    Mejora Movimientos

--- Juego Xipetiaos 2.0/Assets/Scripts/player.cs
+++ Juego Xipetiaos 2.0/Assets/Scripts/player.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    public float velocidadPlayer = 0.1f;
-
     private Animator animator;
+    public Rigidbody2D playerRigidbody2D;
+    Vector2 move;
+    public float speed = 150;
 
 
     void Awake()
@@ -17,13 +18,19 @@ public class player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        playerRigidbody2D = GetComponent<Rigidbody2D>();
+    }
 
+    void FixedUpdate()
+    {
+        playerRigidbody2D.velocity = move * speed * Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Caminar();
+        move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        CaminarAnimacion();
         Correr();
     }
 
@@ -31,66 +38,70 @@ public class player : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.LeftShift))
         {
-            transform.Translate(-velocidadPlayer, 0, 0);
+            speed = 300;
             animator.SetBool("correrIzquierda", true);
             animator.SetBool("caminarIzquierda", false);
         }
-        if (Input.GetKeyUp(KeyCode.A))
-        {
-            animator.SetBool("correrIzquierda", false);
-        }
         if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.LeftShift))
         {
-            transform.Translate(+velocidadPlayer, 0, 0);
+            speed = 300;
             animator.SetBool("correrDerecha", true);
             animator.SetBool("caminarDerecha", false);
+        }
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 300;
+            animator.SetBool("correrArriba", true);
+            animator.SetBool("caminarArriba", false);
+        }
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 300;
+            animator.SetBool("correrAbajo", true);
+            animator.SetBool("caminarAbajo", false);
+        }
+        if (Input.GetKeyUp(KeyCode.A)) 
+        {
+            animator.SetBool("correrIzquierda", false);
         }
         if (Input.GetKeyUp(KeyCode.D))
         {
             animator.SetBool("correrDerecha", false);
         }
-        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
-        {
-            transform.Translate(0, +velocidadPlayer, 0);
-            animator.SetBool("correrArriba", true);
-            animator.SetBool("caminarArriba", false);
-        }
         if (Input.GetKeyUp(KeyCode.W))
         {
             animator.SetBool("correrArriba", false);
-        }
-        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.LeftShift))
-        {
-            transform.Translate(0, -velocidadPlayer, 0);
-            animator.SetBool("correrAbajo", true);
-            animator.SetBool("caminarAbajo", false);
         }
         if (Input.GetKeyUp(KeyCode.S))
         {
             animator.SetBool("correrAbajo", false);
         }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            animator.SetBool("correrAbajo", false);
+            animator.SetBool("correrArriba", false);
+            animator.SetBool("correrIzquierda", false);
+            animator.SetBool("correrDerecha", false);
+            speed = 150;
+        }
     }
 
-    public void Caminar()
+    public void CaminarAnimacion()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(-velocidadPlayer, 0, 0);
             animator.SetBool("caminarIzquierda", true);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(+velocidadPlayer, 0, 0);
             animator.SetBool("caminarDerecha", true);
         }
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(0, +velocidadPlayer, 0);
             animator.SetBool("caminarArriba", true);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(0, -velocidadPlayer, 0);
             animator.SetBool("caminarAbajo", true);
         }
         if (Input.GetKeyUp(KeyCode.W))
