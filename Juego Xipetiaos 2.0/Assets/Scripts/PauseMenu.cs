1a1ea2569ff8b30ee4b3f51ff93012dc91d09982
kzeta11 <112991280+kzeta11@users.noreply.github.com>
2022-11-06 21:46:08 -0300

    IMPLEMENTACION DE BOTON PAUSE

--- Juego Xipetiaos 2.0/Assets/Scripts/PauseMenu.cs
+++ Juego Xipetiaos 2.0/Assets/Scripts/PauseMenu.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenu;
+    private GameObject pauseButton;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            Time.timeScale = 0f;
             this.pauseMenu.SetActive(true);
         }
     }
+    public void ButtonBack()
+    {
+        Time.timeScale = 1f;
+        this.pauseMenu.SetActive(false);
+    }
 }
+
