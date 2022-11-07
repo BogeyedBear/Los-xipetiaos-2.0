c17f91e1af8694826069ee3193487303b7737e68
BaljeetTM <nicolas.tapia08@alumnos.ucn.cl>
2022-11-07 18:10:40 -0300

    Pathfinding 2

--- Juego Xipetiaos 2.0/Assets/Scripts/Asesino.cs
+++ Juego Xipetiaos 2.0/Assets/Scripts/Asesino.cs
@@ -1,14 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class Asesino : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public AIPath aiPath;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -22,6 +20,12 @@ public class Asesino : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }else if (aiPath.desiredVelocity.x >= -0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
     }
 }
