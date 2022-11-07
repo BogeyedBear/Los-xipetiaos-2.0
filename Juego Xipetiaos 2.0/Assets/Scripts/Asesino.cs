97a1e120be4a1aba901dbeced639ec3f2ba204f6
BaljeetTM <nicolas.tapia08@alumnos.ucn.cl>
2022-11-07 19:20:28 -0300

    KillerAnimation

--- Juego Xipetiaos 2.0/Assets/Scripts/Asesino.cs
+++ Juego Xipetiaos 2.0/Assets/Scripts/Asesino.cs
@@ -7,6 +7,12 @@ public class Asesino : MonoBehaviour
 {
 
     public AIPath aiPath;
+    private Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -22,10 +28,101 @@ public class Asesino : MonoBehaviour
     {
         if (aiPath.desiredVelocity.x >= 0.01f)
         {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-        }else if (aiPath.desiredVelocity.x >= -0.01f)
+            //transform.localScale = new Vector3(1f, 1f, 1f);
+            animator.SetBool("arribaE", false);
+            animator.SetBool("izquierdaE", false);
+            animator.SetBool("abajoE", false);
+            animator.SetBool("derechaE", true);
+
+        }
+        else if (aiPath.desiredVelocity.x <= -0.01f)
         {
-            transform.localScale = new Vector3(1f, 1f, 1f);
+            //transform.localScale = new Vector3(1f, 1f, 1f);
+            animator.SetBool("arribaE", false);
+            animator.SetBool("izquierdaE", true);
+            animator.SetBool("abajoE", false);
+            animator.SetBool("derechaE", false);
+        }
+        else if (aiPath.desiredVelocity.y >= 0.01f)
+        {
+            //transform.localScale = new Vector3(1f, 1f, 1f);
+            animator.SetBool("arribaE", true);
+            animator.SetBool("izquierdaE", false);
+            animator.SetBool("abajoE", false);
+            animator.SetBool("derechaE", false);
+        }
+        else if (aiPath.desiredVelocity.y <= -0.01f)
+        {
+            //transform.localScale = new Vector3(1f, 1f, 1f);
+            animator.SetBool("arribaE", false);
+            animator.SetBool("izquierdaE", false);
+            animator.SetBool("abajoE", true);
+            animator.SetBool("derechaE", false);
+        }
+        //abajo();
+        //arriba();
+        //derecha();
+        //izquierda();
+        /**if (aiPath.desiredVelocity.y >= 0.01f)
+        {
+            //transform.localScale = new Vector3(1f, 1f, 1f);
+            animator.SetBool("arribaE", true);
+            animator.SetBool("izquierdaE", false);
+            animator.SetBool("abajoE", false);
+            animator.SetBool("derechaE", false);
+        }
+        if (aiPath.desiredVelocity.y <= -0.01f)
+        {
+            //transform.localScale = new Vector3(1f, 1f, 1f);
+            animator.SetBool("arribaE", false);
+            animator.SetBool("izquierdaE", false);
+            animator.SetBool("abajoE", true);
+            animator.SetBool("derechaE", false);
+        }**/
+    }
+
+    private void abajo()
+    {
+        if (aiPath.desiredVelocity.y <= -0.01f)
+        {
+            //transform.localScale = new Vector3(1f, 1f, 1f);
+            animator.SetBool("arribaE", false);
+            animator.SetBool("izquierdaE", false);
+            animator.SetBool("abajoE", true);
+            animator.SetBool("derechaE", false);
+        }
+    }
+    private void arriba()
+    {
+        if (aiPath.desiredVelocity.y >= 0.01f)
+        {
+            //transform.localScale = new Vector3(1f, 1f, 1f);
+            animator.SetBool("arribaE", true);
+            animator.SetBool("izquierdaE", false);
+            animator.SetBool("abajoE", false);
+            animator.SetBool("derechaE", false);
+        }
+    }
+    private void derecha()
+    {
+        if (aiPath.desiredVelocity.x >= 0.01f)
+        {
+            //transform.localScale = new Vector3(-1f, 1f, 1f);
+            animator.SetBool("arribaE", false);
+            animator.SetBool("izquierdaE", false);
+            animator.SetBool("abajoE", false);
+            animator.SetBool("derechaE", true);
+        }
+    }
+    private void izquierda()
+    {
+        if (aiPath.desiredVelocity.x <= -0.01f)
+        {
+            //transform.localScale = new Vector3(-1f, 1f, 1f);
+            animator.SetBool("arribaE", false);
+            animator.SetBool("izquierdaE", true);
+            animator.SetBool("abajoE", false);
+            animator.SetBool("derechaE", false);
         }
     }
 }
